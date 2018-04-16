779f2f047974d2b0ae57f3a24666c71d81185801
Othman <TheWeeknd@Othmans-MacBook-Pro.local>
2018-04-16 14:57:22 +0300

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: { maximum: 19 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 25 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+end
